911d5ab2c6206e9f34e357253b17e4e4e15032af
Roshini <sroshini24082gmail.com>
2021-05-22 23:57:34 +0530

    navbar js

--- src/components/function/Navbar.js
+++ src/components/function/Navbar.js
@@ -1,14 +1,23 @@
  import React from 'react'
  import './Navbar.css'
+ import { Link } from "react-router-dom"
  export default function Navbar() {
    return (
-     <div>
-       <ul>
-         <li><a className="active" href="#home">Home</a></li>
-         <li><a href="#update">Update</a></li>
-         <li><a href="#about">About</a></li>
-       </ul>
+     <div className="body">
+       <nav>
+         <Link to="/"  >Home</Link>
+         <Link to="/login">Hospital-login</Link>
+         <Link to="/about">About</Link>
+         <Link to="/contact">Whom to Contact</Link>
+         <div className="animation start-home"></div>
+        </nav>
      </div>
    )
  }
+ /*<ul>
+         <li><Link to="/" className="active" >Home</Link></li>
+         <li><a href="#update">Hospital login</a></li>
+         <li><Link to="/about">About</Link></li>
+         <li><Link to="/contact">Whom to Contact</Link></li>
+       </ul>*/
  
---
+++ src/components/function/Searchbar.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import './Searchbar.css'
+
+export default function Searchbar() {
+  return (
+    
+    <div className="search">
+        <i className="fas fa-search"></i>
+        <input type="text" placeholder="Search by Hospital/Care Centre Name"></input>
+
+    </div>
+    
+  )
+}
