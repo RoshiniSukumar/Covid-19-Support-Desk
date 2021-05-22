fd0a1ef791c3b4a92fd62e15b6468b6891bf6bb4
Roshini <sroshini24082gmail.com>
2021-05-22 23:58:37 +0530

    home js

---
+++ src/components/function/Home.js
@@ -0,0 +1,39 @@
+import React from 'react'
+import './Home.css'
+ 
+export default function Home() {
+  return (
+    <div className="fling-minislide">
+      <img src='./images/image1.jpg' alt="image1" width="700" height="auto"></img>
+      <img src='./images/image2.jpg' alt="image2" width= "500" height="auto"></img>
+      <img src='./images/image3.jpg' alt="image3" width="700" height="auto"></img>
+    </div>
+    
+      
+    
+      
+    
+  )
+}
+ /*<div>
+      <span className="dot"></span>
+      <span className="dot"></span>
+      <span className="dot"></span>
+    </div>
+    */
+   /*<div className="container ">
+    <div className="slideshow_wrapper">
+      <div className="slidshow">
+        <div className="slide_one slide"></div>
+      <img src='./images/image1.jpg' alt="image1"></img></div>
+    
+
+    <div className="slide_two slide">
+       <img src='./images/image2.jpg'alt="image2"></img></div>
+    
+    
+    <div className="slide_three slide">
+      <img src='./images/image3.jpg' alt="image3"></img></div>
+     
+         </div>
+       </div>*/
