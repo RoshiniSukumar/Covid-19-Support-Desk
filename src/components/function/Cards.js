a1bcd5d48ec394d94ba2788fbbfab0ca79b865a1
Roshini <sroshini24082gmail.com>
2021-05-25 22:42:44 +0530

    card js

--- src/components/function/Cards.js
+++ src/components/function/Cards.js
@@ -2,23 +2,41 @@ import React from 'react'
 
 export default function Cards() {
   return (
-    <div>
-      <div className="containers">
-          <div className="card">
-              <div className="circle">
-              </div>
-              <div className="content">
-                  <h2>CHO - ARUN HOSPITAL</h2>
+    
+      <div className="boxs">
+          <div className="figure">
+          <h2>CHO - ARUN HOSPITAL</h2>
+              <div className="caption">
+              
+              <div className="about">
+                  
                   <p>NEW SCHEME ROAD, Pollachi
                     Coimbatore
                   </p>
-                  <h6 className="count">Vacant/Total</h6>
-                  <ul>
-                      <li>Normal: 10/46</li>
-                      <li>Oxygen:0/32</li>
-                      <li>ICU:  0/2</li>
-                      <li>total: 10/70</li>
-                  </ul>
+                   
+                  <table>
+                    <tr>
+                  <th>Bed type</th>
+                  <th>Vacant/Total</th>
+                  </tr>
+                  <tr>
+                    <td>Normal</td>
+                    <td>10/46</td>
+                    </tr>
+                    <tr><td>Oxygen</td>
+                    <td>0/32</td>
+                    </tr>
+                    <tr><td>ICU</td>
+                    <td>0/2</td>
+                      </tr> 
+                      <tr>
+                        <td>Total</td>
+                        <td>10/70</td>
+                      </tr>
+
+
+
+                  </table>
               </div>
 
           </div>
