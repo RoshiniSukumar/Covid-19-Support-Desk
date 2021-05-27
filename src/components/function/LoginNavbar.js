67af17bc55f36ebb2d479cdc19a64ec873e9747a
Roshini <sroshini24082gmail.com>
2021-05-27 23:32:41 +0530

    loginnavbar js

---
+++ src/components/function/LoginNavbar.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import './LoginNavbar.css'
+
+export default function LoginNavbar() {
+  return (
+       
+     
+      <div class="nav">
+         
+            <ol>
+                <li><a href="/loginform">Login</a></li>
+                <li><a href="/registerform">Register</a></li>
+                 
+            </ol>
+        
+        </div>
+  )
+}
+
