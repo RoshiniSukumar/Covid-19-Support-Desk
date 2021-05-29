18c0c16e0a01b8ed34497b79d193bcc9b717ee9a
Roshini <sroshini24082gmail.com>
2021-05-29 23:11:37 +0530

    loginnavbar js

--- src/components/function/LoginNavbar.js
+++ src/components/function/LoginNavbar.js
@@ -12,11 +12,28 @@ export default function LoginNavbar() {
                 <li><a href="/registerform">Register</a></li>
                  
             </ol>
-        
-        </div>
-        <div className="gifs">
-         <img src='./images/tenor.gif' alt="tenor" width="300" height="300"></img>
-        </div>
+            </div>
+            <div className="video">
+
+            <iframe width="560" height="315" src="https://www.youtube.com/embed/cQBo6Bbk0_A" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+              </div>
+               <div className="full">
+              <div className="sidelinks"> 
+       <div className="other1"><a href=" https://ereceipt.tn.gov.in/Cmprf/Cmprf">CM Welfare Fund</a></div>
+         </div>
+
+         <div className="sidelinks"> 
+       <div className="other2"><a href=" https://eregister.tnega.org/#/user/pass">To Apply E-Pass</a></div>
+         </div>
+
+         <div className="sidelinks"> 
+       <div className="other3"><a href="https://stopcorona.tn.gov.in/vaccine_center.php">Vaccine Centres</a></div>
+         </div>
+
+         <div className="sidelinks"> 
+       <div className="other4"><a href=" https://stopcorona.tn.gov.in/sample_collection_center.php">Sample Collection Centres</a></div>
+         </div>
+         </div>
         </div> 
         
   )
