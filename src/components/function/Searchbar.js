ad85931eb84262b8e6bbdaca7c2cbb86fa218f5b
Roshini <sroshini24082gmail.com>
2021-06-06 23:24:22 +0530

    searchbar js

--- src/components/function/Searchbar.js
+++ src/components/function/Searchbar.js
@@ -1,12 +1,13 @@
 import React from 'react'
 import './Searchbar.css'
-
+ 
+ 
 export default function Searchbar() {
   return (
     
     <div className="search">
-        <i className="fas fa-search"></i>
-        <input type="text" placeholder="Search by Hospital/Care Centre Name"></input>
+        <i className="fa-search"></i>
+        <input type="text1" placeholder="Search by Hospital/Care Centre Name"></input>
 
     </div>
     
