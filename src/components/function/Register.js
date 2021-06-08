fe03c6622defc13def72f7495daf37993a9bddbe
Roshini <sroshini24082gmail.com>
2021-06-08 23:05:46 +0530

    register js

--- src/components/function/Register.js
+++ src/components/function/Register.js
@@ -1,192 +1,236 @@
 import React from 'react'
-import { useState } from "react";
-import { Formik, Form, Field, ErrorMessage } from "formik";
-import * as Yup from "yup";
-import './Register.css'
+import {Formik,useFormik} from "formik";
+
+
+const initialValues = {
+  email:"",
+  password:"",
+  confirmpassword:""
+};
+const validate = (values) => {
+  let error = {};
+ 
+  if (!values.email) {
+    error.email = "* Required Field";
+  }
+  if (!values.password) {
+    error.password = "* Required Field";
+  }
+  if (values.confirmpassword!='password') {
+    error.confirmpassword = "* Passwords must match!";
+    elseif(!values.confirmpassword){
+      error.confirmpassword = "* Required Field";
+    }
+  }
+
+  return error;
+};
+
+function Register(props){
+  const onSubmit = async (values) => {
+    values.add = {
+      email: values.email,
+      password: values.password,
+      confirmpassword:values.confirmpassword
+    };
+
+    console.log(values.add)
+  };
+  const formik = useFormik({
+    initialValues,
+    onSubmit,
+    validate,
+  });
+  
+}
+// import { useState } from "react";
+// import { Formik, Form, Field, ErrorMessage } from "formik";
+// import * as Yup from "yup";
+// import './Register.css'
  
 
-export default function Register() {
+// export default function Register() {
    
-  const [data, setData] = useState({
-    email: "",
-    password: "",
-    confirmpassword:"",
-    hospitaltype:"",
-    hospitalname:"",
-    address:"",
-    contactnumber:"",
-    normalbed:"",
-    icubed:"",
-    o2bed:"",
-    total:""
+//   const [data, setData] = useState({
+//     email: "",
+//     password: "",
+//     confirmpassword:"",
+//     hospitaltype:"",
+//     hospitalname:"",
+//     address:"",
+//     contactnumber:"",
+//     normalbed:"",
+//     icubed:"",
+//     o2bed:"",
+//     total:""
 
-  });
-  const [currentStep, setCurrentStep] = useState(0);
-  const [errors, setErrors] = useState({});
+//   });
+//   const [currentStep, setCurrentStep] = useState(0);
+//   const [errors, setErrors] = useState({});
 
-  const makeRequest = (formData) => {
-    console.log("Form Submitted", formData);
-  };
+//   const makeRequest = (formData) => {
+//     console.log("Form Submitted", formData);
+//   };
 
-  const handleNextStep = (newData, final = false) => {
-    setData((prev) => ({ ...prev, ...newData }));
+//   const handleNextStep = (newData, final = false) => {
+//     setData((prev) => ({ ...prev, ...newData }));
 
-    if (final) {
-      makeRequest(newData);
-      return;
-    }
+//     if (final) {
+//       makeRequest(newData);
+//       return;
+//     }
 
-    setCurrentStep((prev) => prev + 1);
-  };
+//     setCurrentStep((prev) => prev + 1);
+//   };
 
-  const handlePrevStep = (newData) => {
-    setData((prev) => ({ ...prev, ...newData }));
-    setCurrentStep((prev) => prev - 1);
-  };
+//   const handlePrevStep = (newData) => {
+//     setData((prev) => ({ ...prev, ...newData }));
+//     setCurrentStep((prev) => prev - 1);
+//   };
    
 
 
-  const steps = [
-    <StepOne next={handleNextStep} data={data} errors={errors} />,
-    <StepTwo next={handleNextStep} prev={handlePrevStep} data={data} />
-  ];
+//   const steps = [
+//     <StepOne next={handleNextStep} data={data} errors={errors} />,
+//     <StepTwo next={handleNextStep} prev={handlePrevStep} data={data} />
+//   ];
 
-  console.log("data", data);
+//   console.log("data", data);
 
-  return <div className="App">{steps[currentStep]}</div>;
-}
+//   return <div className="App">{steps[currentStep]}</div>;
+// }
 
-const stepOneValidationSchema = Yup.object({
-  email: Yup.string().email('Invalid email format').required('*Required').label("Email"),
-  password: Yup.string().required('*Required').label("Password"),
-  confirmpassword:Yup.string().oneOf([Yup.ref('password'),''],'*Passwords must match').required('*Required').label("Confirm Password")
-});
+// const stepOneValidationSchema = Yup.object({
+//   email: Yup.string().email('Invalid email format').required('*Required').label("Email"),
+//   password: Yup.string().required('*Required').label("Password"),
+//   confirmpassword:Yup.string().oneOf([Yup.ref('password'),''],'*Passwords must match').required('*Required').label("Confirm Password")
+// });
 
-const StepOne = (props) => {
-  const handleSubmit = (values) => {
-    props.next(values);
-  };
-  // props.addDetails(data);
-  // console.log(data);
+// const StepOne = (props) => {
+//   const handleSubmit = (values) => {
+//     props.next(values);
+//   };
+//   // props.addDetails(data);
+//   // console.log(data);
 
-  return (
-    <Formik
-      validationSchema={stepOneValidationSchema}
-      initialValues={props.data}
-      onSubmit={handleSubmit}
-    >
-       {({ values }) => (
-        <div className="contain">
-        <Form id="form1">
-          <h3>Create Account</h3>
-          <p className="lbl">Email</p>
-          <Field name="email" />
-          <div className="required">
-          <ErrorMessage name="email" />
+//   return (
+//     <Formik
+//       validationSchema={stepOneValidationSchema}
+//       initialValues={props.data}
+//       onSubmit={handleSubmit}
+//     >
+//        {({ values }) => (
+//         <div className="contain">
+//         <Form id="form1">
+//           <h3>Create Account</h3>
+//           <p className="lbl">Email</p>
+//           <Field name="email" />
+//           <div className="required">
+//           <ErrorMessage name="email" />
            
-          </div>
+//           </div>
           
           
 
-          <p className="lbl">Password</p>
-          <Field name="password" type="password" />
-          <div className="required">
-          <ErrorMessage name="password" /></div>
+//           <p className="lbl">Password</p>
+//           <Field name="password" type="password" />
+//           <div className="required">
+//           <ErrorMessage name="password" /></div>
 
-          <p className="lbl">Confirm Password</p>
-          <Field name="confirmpassword" type="password"/>
-          <div className="required">
-          <ErrorMessage name="confirmpassword" /></div>
+//           <p className="lbl">Confirm Password</p>
+//           <Field name="confirmpassword" type="password"/>
+//           <div className="required">
+//           <ErrorMessage name="confirmpassword" /></div>
            
-           <div className="btn-box">
-          <button type="submit">Next</button>
-          </div>
-        </Form>
-        </div>
-      )}
-    </Formik>
-  );
-};
+//            <div className="btn-box">
+//           <button type="submit">Next</button>
+//           </div>
+//         </Form>
+//         </div>
+//       )}
+//     </Formik>
+//   );
+// };
 
-const stepTwoValidationSchema = Yup.object({
-  hospitaltype:Yup.string().required('*Required'),
-  hospitalname: Yup.string().required('*Required').label("Name of Hospital/Centre"),
-  address: Yup.string().required('*Required').label("Address"),
-  contactnumber:Yup.number().required('*Required').label("Contact Number"),
-  normalbed: Yup.string().required('*Required').label("Normal Beds"),
-  icubed:Yup.string().required('*Required').label("ICU Beds"),
-  o2bed: Yup.string().required('*Required').label("Oxygen Beds"),
-  total: Yup.string().required('*Required').label("Total"),
+// const stepTwoValidationSchema = Yup.object({
+//   hospitaltype:Yup.string().required('*Required'),
+//   hospitalname: Yup.string().required('*Required').label("Name of Hospital/Centre"),
+//   address: Yup.string().required('*Required').label("Address"),
+//   contactnumber:Yup.number().required('*Required').label("Contact Number"),
+//   normalbed: Yup.string().required('*Required').label("Normal Beds"),
+//   icubed:Yup.string().required('*Required').label("ICU Beds"),
+//   o2bed: Yup.string().required('*Required').label("Oxygen Beds"),
+//   total: Yup.string().required('*Required').label("Total"),
     
 
-});
+// });
 
-const StepTwo = (props) => {
-  const handleSubmit = (values) => {
-    props.next(values, true);
-  };
+// const StepTwo = (props) => {
+//   const handleSubmit = (values) => {
+//     props.next(values, true);
+//   };
 
-  return (
-    <Formik
-      validationSchema={stepTwoValidationSchema}
-      initialValues={props.data}
-      onSubmit={handleSubmit}
-    >
-      {({ values }) => (
-       <div className="contain1">
-        <Form id="form2">
-          <h3>Centres Details</h3>
-          <p className="lbl">Type of Hospital/Centre</p>
-          <Field name="hospitaltype" />
-          <div className="required">
-          <ErrorMessage name="hospitaltype" /></div>
+//   return (
+//     <Formik
+//       validationSchema={stepTwoValidationSchema}
+//       initialValues={props.data}
+//       onSubmit={handleSubmit}
+//     >
+//       {({ values }) => (
+//        <div className="contain1">
+//         <Form id="form2">
+//           <h3>Centres Details</h3>
+//           <p className="lbl">Type of Hospital/Centre</p>
+//           <Field name="hospitaltype" />
+//           <div className="required">
+//           <ErrorMessage name="hospitaltype" /></div>
           
-          <p className="lbl">Name of Hospital/Centre</p>
-          <Field name="hospitalname" />
-          <div className="required">
-          <ErrorMessage name="hospitalname" /></div>
+//           <p className="lbl">Name of Hospital/Centre</p>
+//           <Field name="hospitalname" />
+//           <div className="required">
+//           <ErrorMessage name="hospitalname" /></div>
 
-          <p className="lbl" >Address</p>
-          <Field name="address" />
-          <div className="required">
-          <ErrorMessage name="address" /></div>
+//           <p className="lbl" >Address</p>
+//           <Field name="address" />
+//           <div className="required">
+//           <ErrorMessage name="address" /></div>
 
-          <p className="lbl" >Contact number</p>
-          <Field name="contactnumber" />
-          <div className="required">
-          <ErrorMessage name="contactnumber" /></div>
+//           <p className="lbl" >Contact number</p>
+//           <Field name="contactnumber" />
+//           <div className="required">
+//           <ErrorMessage name="contactnumber" /></div>
 
-          <p className="lbl">Normal Beds</p>
-          <Field name="normalbed" />
-          <div className="required">
-          <ErrorMessage name="normalbed" /></div>
+//           <p className="lbl">Normal Beds</p>
+//           <Field name="normalbed" />
+//           <div className="required">
+//           <ErrorMessage name="normalbed" /></div>
 
-          <p className="lbl">ICU Beds</p>
-          <Field name="icubed" />
-          <div className="required">
-          <ErrorMessage name="icubed" /></div>
+//           <p className="lbl">ICU Beds</p>
+//           <Field name="icubed" />
+//           <div className="required">
+//           <ErrorMessage name="icubed" /></div>
   
-          <p className="lbl">Oxygen Beds</p>
-          <Field name="o2bed" />
-          <div className="required">
-          <ErrorMessage name="o2bed" /></div>
+//           <p className="lbl">Oxygen Beds</p>
+//           <Field name="o2bed" />
+//           <div className="required">
+//           <ErrorMessage name="o2bed" /></div>
 
-          <p className="lbl">Total</p>
-          <Field name="total" />
-          <div className="required">
-          <ErrorMessage name="total" /></div>
+//           <p className="lbl">Total</p>
+//           <Field name="total" />
+//           <div className="required">
+//           <ErrorMessage name="total" /></div>
           
           
-          <div className="btn-box">
-          <button type="button" onClick={() => props.prev(values)}>
-            Back
-          </button>
-          <button type="submit">Submit</button>
-          </div>
+//           <div className="btn-box">
+//           <button type="button" onClick={() => props.prev(values)}>
+//             Back
+//           </button>
+//           <button type="submit">Submit</button>
+//           </div>
           
-        </Form>
-        </div>
-      )}
-    </Formik>
-  );
-};
+//         </Form>
+//         </div>
+//       )}
+//     </Formik>
+//   );
+// };
