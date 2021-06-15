b5fdbfbd2d2e4687718d7c2c718989dc309442b1
Roshini <sroshini24082gmail.com>
2021-06-15 22:59:17 +0530

    app js

--- src/App.js
+++ src/App.js
@@ -7,17 +7,17 @@ import Checkbox from './components/function/Checkbox'
 import Searchbar from './components/function/Searchbar'
 import Contact from './components/function/Contact'
 import Home from './components/function/Home'
-import Exlinks from './components/function/Exlinks'
-import Login from './components/function/Login'
+import Exlinks from './components/function/Exlinks' 
 import Links from './components/function/Links'
 import About from './components/function/About'
-import Register from './components/function/Register'
 import Socialmediabar from './components/function/Socialmediabar'
 import {BrowserRouter,Route,Switch} from "react-router-dom"
 import BedDetails from './components/function/BedDetails'
  import FormDetails from './components/function/FormDetails'
 import Tracker from  './components/function/Tracker'
 import Signup from './components/function/Signup'
+import Signin from './components/function/Signin'
+import MyDetails from './components/function/MyDetails'
 
 function App() {
 //     const [details,setDetails] = useState([]);
@@ -96,21 +96,25 @@ total:"30/36"
       <Route exact path="/about">
         <About/>
         <Exlinks/>
-        
+        <MyDetails/>
       </Route>
 
       <Route exact path="/login">
 
          <p className="condition">***This page is for hospital use only!</p>
        
-        <Login/> 
-        
-              
+         <Signin/> 
         </Route>
+              
+        
+      
+    
+    
         <Route exact path="/register" > 
+        <p className="condition">***This page is for hospital use only!</p>
         <Signup/>
         </Route>
-
+        
         <Route exact path="/register/details" > 
         <FormDetails/>
         </Route>
