f243b54b6a77f0974529e64faa5ce55b81895f07
Roshini <sroshini24082gmail.com>
2021-06-18 22:45:53 +0530

    navbar js

--- src/components/function/Navbar.js
+++ src/components/function/Navbar.js
@@ -7,7 +7,7 @@
        <nav>
         
          <Link to="/">Home</Link>
-         <Link Link to="/tracker">Covid Tracker</Link>
+         <Link to="/tracker">Covid Tracker</Link>
          <Link to="/login">Hospital-login</Link>
          <Link to="/about">About</Link>
          <Link to="/contact">Whom to Contact</Link>
